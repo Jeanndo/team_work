dfe6d8d158c59d59a55674064fdebd93c7be0813
Jeanndo <ukjeando@gmail.com>
2021-02-16 10:47:13 +0200

    autho controller updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,4 @@
-import Employee from '../models/employeeModel';
+import User from '../models/userModel';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/appError'
 import jwt from 'jsonwebtoken'
@@ -6,22 +6,28 @@ import jwt from 'jsonwebtoken'
 export const signUp = catchAsync(async (req,res,next)=>{
 
     //const employee = await Employee.create(req.body);
-    const employee = await Employee.create({
-        name: req.body.name,
+    const user = await User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
         email:req.body.email,
         password:req.body.password,
-        confirmPassword:req.body.confirmPassword
+        confirmPassword:req.body.confirmPassword,
+        gender:req.body.gender,
+        jobRole:req.body.gender,
+        department:req.body.gender,
+        address:req.body.address
     });
 
-    const token = jwt.sign({id:employee._id},process.env.JWT_SECRET,{
+    const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{
         expiresIn:process.env.JWT_EXPIRES_IN
     })
     
     res.status(201).json({
         status:'success',
+        message:'User created successfully',
         token,
         data:{
-            employee 
+            user 
         }
     })
 })
@@ -35,7 +41,8 @@ export const login = catchAsync(async(req,res,next)=>{
     return next(new AppError("Please provide an email and password",400));
     }
      
-    const employee = await Employee.findOne({email}).select('+password')
+    const user = await User.findOne({email}).select('+password')
+    console.log(user);
     const token  = '';
 
     res.status(202).json({
