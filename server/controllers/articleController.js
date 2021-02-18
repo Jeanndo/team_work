e2ad36d9f9fd23a3bba68572621a870783ca1f91
Odile <odille1998@gmail>
2021-02-18 20:38:39 +0200

    articlecontroller

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -1,1 +1,65 @@
 
+import Article from '../models/articleModel';
+import catchAsync from '../utils/catchAsync';
+
+
+export const createArticle = catchAsync(async (req,res,next)=>{
+
+  const newArticle = await Article.create(req.body);
+
+  res.status(201).json({
+      status:'success',
+      data:{
+          newArticle
+      }
+  })
+})
+ 
+export const getAllArticles = catchAsync(async (req,res,next)=>{
+
+  const articles = await Article.find();
+
+  res.status(200).json({
+    status:'success',
+    data:{
+      articles
+    }
+  })
+})
+
+
+export const getArticle = catchAsync(async (req,res,next)=>{
+
+  const article= await Article.findById(req.params.id)
+
+  res.status(200).json({
+      status:'success',
+      data:{
+          article
+      }
+    })
+})
+
+
+export const deleteArticle =  catchAsync(async (req,res,next)=>{
+
+  const article = await Article.findByIdAndDelete(req.params.id)
+
+  res.status(200).json({
+      status:'success',
+      data:{}
+    })
+})
+
+export const updateArticle = catchAsync(async (req,res,next)=>{
+
+    const article=await Article.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidator:true
+    })
+    res.status(200).json({
+        success:true,
+        data: article
+    })
+
+})
