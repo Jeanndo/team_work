dc00af0e950d65a457c545368d5e1a504727a30b
UKWITEGETSE JEAN DE DIEU <59208992+Jeanndo@users.noreply.github.com>
2021-02-23 14:41:44 +0200

    Update commentModel.js

---
+++ server/models/commentModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+ const commentSchemma = new mongoose.Schema({
+
+    comment:{
+        type:String,
+        maxlength:100
+    },
+    articleID:{
+        type:String
+    }
+})
+
+const Comment = mongoose.model('Comment',commentSchemma);
+
+export default Comment
