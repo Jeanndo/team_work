39fd07f5888d99074fb09a439b82f54cb342ffcd
Odile <odille1998@gmail>
2021-02-23 14:51:36 +0200

    Associating article post with comment

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -1,7 +1,7 @@
 
 import Article from '../models/articleModel';
 import catchAsync from '../utils/catchAsync';
-
+import Comment from '../models/commentModel';
 
 export const createArticle = catchAsync(async (req,res,next)=>{
 
@@ -38,11 +38,16 @@ export const getAllArticles = catchAsync(async (req,res,next)=>{
 export const getArticle = catchAsync(async (req,res,next)=>{
 
   const article= await Article.findById(req.params.id)
+  let comment=await Comment.find();
+  const myComment =comment.filter(el=>{
+    return el.articleId===req.params.id
+  })
 
   res.status(200).json({
       status:'success',
       data:{
-          article
+          article,
+          myComment
       }
     })
 })
